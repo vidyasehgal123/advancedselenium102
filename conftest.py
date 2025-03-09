d1699b8d64d47c0eefa13ff78edcde9b91f6d554
Vidya Sehgal <43105422+vidyasehgal123@users.noreply.github.com>
2025-03-09 16:02:31 -0400

    Add files via upload

---
+++ conftest.py
@@ -0,0 +1,62 @@
+from os import environ
+
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture(scope='function')
+def driver(request):
+# parallize_test_accross_combinations
+    desired_caps = {}
+    browser = request.param
+
+    desired_caps.update(browser)
+    test_name = request.param["platform"] + "_" + request.param["browserName"] + "_" + request.param["version"]
+    build = environ.get('BUILD', "Sample PY Build Chrome")
+
+    tunnel_id = environ.get('TUNNEL', False)
+    username ="vidya_sehgal"
+    access_key ="iLSg7GQ9o7VmQY2klGpwM5FTrB8LGfyId3c3pZpZE67922SbBC"
+
+    selenium_endpoint = "http://{}:{}@hub.lambdatest.com/wd/hub".format(username, access_key)
+    chrome_options = webdriver.ChromeOptions()
+    option = {
+        "platform": "Windows 10",
+        "version": "latest",
+        "name": test_name,
+        "Build": build,
+        "video": True,
+        "visual": True,
+        "network": True,
+        "console": True
+    }
+    chrome_options.set_capability("LT:Options", option)
+    browser = webdriver.Remote(
+        command_executor=selenium_endpoint,
+        options=chrome_options
+    )
+    yield browser
+
+    def fin():
+        # browser.execute_script("lambda-status=".format(str(not request.node.rep_call.failed if "passed" else
+        # "failed").lower()))
+        if request.node.rep_call.failed:
+            browser.execute_script("lambda-status=failed")
+            browser.quit()
+        else:
+            browser.execute_script("lambda-status=passed")
+            browser.quit()
+
+    request.addfinalizer(fin)
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    # this sets the result as a test attribute for LambdaTest reporting.
+    # execute all other hooks to obtain the report object
+    outcome = yield
+    rep = outcome.get_result()
+
+    # set a report attribute for each phase of a call, which can
+    # be "setup", "call", "teardown"
+    setattr(item, "rep_" + rep.when, rep)
